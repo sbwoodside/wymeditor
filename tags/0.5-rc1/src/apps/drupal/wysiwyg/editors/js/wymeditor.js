ffb39fbe3dfa8ee191a773aa1a3426f4faba3688
jf.hovinne <jf.hovinne@89e89e35-0a13-0410-8f61-920bba073fa9>
2009-05-27 19:52:36 +0000

    Created 0.5 rc1 tag.

---
+++ tags/0.5-rc1/src/apps/drupal/wysiwyg/editors/js/wymeditor.js
@@ -0,0 +1,34 @@
+// $Id$
+
+/**
+ * Attach this editor to a target element.
+ */
+Drupal.wysiwyg.editor.attach.wymeditor = function(context, params, settings) {
+  $('#' + params.field).wymeditor(eval(settings));
+};
+
+/**
+ * Detach a single or all editors.
+ */
+Drupal.wysiwyg.editor.detach.wymeditor = function(context, params) {
+  if (typeof params != 'undefined') {
+    var $field = $('#' + params.field);
+    var index = $field.data(WYMeditor.WYM_INDEX);
+    if (typeof index != 'undefined') {
+      var instance = WYMeditor.INSTANCES[index];
+      instance.update();
+      $(instance._box).remove();
+      delete instance;
+    }
+    $field.show();
+  }
+  else {
+    jQuery.each(WYMeditor.INSTANCES, function() {
+      this.update();
+      $(this._box).remove();
+      $(this._element).show();
+      delete this;
+    });
+  }
+};
+
